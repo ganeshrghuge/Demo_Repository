52d4f00545ed64800e4b6938ea5ee5dc7aef9642
ganeshrghuge <ganeshghuge9090@gmail.com>
2020-02-02 13:31:02 +0530

    second commit

--- Demo_project/employee_app/views.py
+++ Demo_project/employee_app/views.py
@@ -11,9 +11,14 @@ class EmployeeAPI():
     def get(self):
         data = Employee.objects.all()
         serializer = EmployeeSerializer(data,many=True)
-        return Response(serializer,status=200)
-    def post(self,request):
-        pass
+        return Response(serializer, data, status=200)
+    def post(self,request,data):
+        data = request.data
+        serializer = EmployeeSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=200)
+        return Response(serializer.errors,status=400)
     def delete(self,request):
         pass
     def put(self,request):
